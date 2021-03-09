1ef8161f45747e1ec969d36fd0d5a843ed4b7e2b
josesrodriguez610 <josesrodriguez610@gmail.com>
2021-03-09 13:43:10 -0600

    (add) finished generateCells logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { CellState, CellValue } from "../../types";
 
 import "./Button.scss";
 
-const Button: React.FC = () => {
-  return <div className="Button"></div>;
+interface ButtonProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.Visible) {
+      if (value === CellValue.Bomb) {
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      } else if (value === CellValue.None) {
+        return null;
+      }
+      return value;
+    } else if (state === CellState.Flagged) {
+      <span role="img" aria-label="flag">
+        🚩
+      </span>;
+    }
+    return null;
+  };
+
+  return (
+    <div
+      className={`Button ${
+        state === CellState.Visible ? "visible" : ""
+      } value-${value}`}
+    >
+      {renderContent()}
+    </div>
+  );
 };
 
 export default Button;
